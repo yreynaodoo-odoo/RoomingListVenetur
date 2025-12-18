7fb221b2b923dd6ec41b4c94884fd8cf96aa3254
yreynaodoo-odoo <yreynaodoo@gmail.com>
2025-12-18 14:59:48 -0400

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -3,7 +3,7 @@
 import { GoogleGenAI, Type } from "@google/genai";
 import type { BookingRecord } from '../types';
 
-const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_API_KEY });
+const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_API_KEY }); 
 
 const responseSchema = {
     type: Type.ARRAY,
